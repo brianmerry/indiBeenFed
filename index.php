4dd5914992c9cdd1a65be51c84184356a1d5ea25
BMerry <admin@Admins-MacBook-Pro.local>
2015-10-26 14:24:19 -0700

    Bug Fixes

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include("include/overall/header.php");
 include("core/init.php");
-echo "<br /><br/><br />";
+echo "<br /><br/><br /><br />";
 	$query= mysql_query("SELECT * FROM `morning` WHERE `fed` ='1'")or die(mysql_error());
 	$arr = mysql_fetch_array($query);
 	$num = mysql_numrows($query); //this will count the rows (if exists) 
