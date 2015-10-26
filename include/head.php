d3d96f07132632ca515ab2e49b25330f0e7fb617
BMerry <admin@Admins-MacBook-Pro.local>
2015-10-26 14:33:21 -0700

    Optimization For Mobile

--- include/head.php
+++ include/head.php
@@ -1,9 +1,9 @@
 	<head>
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="apple-touch-icon" size="57x57" href="http://barney.gonzaga.edu/~bmerry/indi/images/startup.jpg">
-	<link rel="apple-touch-startup-image" href="http://barney.gonzaga.edu/~bmerry/indi/images/startup.jpg">
-	<meta name="apple-mobile-web-app-title" content="Indi!">
+	<link rel="apple-touch-icon" size="57x57" href="images/startup.jpg">
+	<link rel="apple-touch-startup-image" href="images/startup.jpg">
+	<meta name="apple-mobile-web-app-title" content="iFedIndi">
 	<meta name="apple-mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 	<title>Has Indi Been Fed?</title>
