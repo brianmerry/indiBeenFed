72c94cffcb80960bb720dc8a59a00dbf1e060298
BMerry <admin@Admins-MacBook-Pro.local>
2015-10-26 14:27:54 -0700

    Bug Fixes

--- include/header.php
+++ include/header.php
@@ -1,4 +1,4 @@
-		<header id="header">
+		<!--<header id="header">
 			<center><img src="images/indiHeader.png" height="100%"></center>
 			<a href="settings.php"><img src="images/gear.jpg.png" height="29px" width="29px" style="margin: -5.25% 6% 100% 94%"></a>
-		</header>
+		</header>-->
