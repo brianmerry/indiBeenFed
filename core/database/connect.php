79d2e53c18850e0106510a774032b2713bdc5277
BMerry <admin@Admins-MacBook-Pro.local>
2015-10-26 13:03:47 -0700

    Correct DB Connection

--- core/database/connect.php
+++ core/database/connect.php
@@ -1,6 +1,6 @@
 <?php
 $connect_error = "Sorry, We've experienced some technical difficulties.";
-$dbLocalhost=mysql_connect('localhost','root','usbw');
-mysql_select_db('indifood') or die($connect_error);
-$connect=mysqli_connect('localhost','root','usbw');
+$dbLocalhost=mysql_connect('us-cdbr-azure-west-c.cloudapp.net:3306','bd377ee286e9fc','4e7a8a97');
+mysql_select_db('indiApp') or die($connect_error);
+$connect=mysqli_connect('us-cdbr-azure-west-c.cloudapp.net:3306','bd377ee286e9fc','4e7a8a97');
 ?>
