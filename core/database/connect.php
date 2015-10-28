a8f90abe6e5cf413b80b8a58be473e604d8dc7cc
BMerry <admin@Admins-MacBook-Pro.local>
2015-10-28 11:08:09 -0700

    Bug Fixes

--- core/database/connect.php
+++ core/database/connect.php
@@ -2,5 +2,5 @@
 $connect_error = "Sorry, We've experienced some technical difficulties.";
 $dbLocalhost=mysql_connect('us-cdbr-azure-west-c.cloudapp.net:3306','bd377ee286e9fc','4e7a8a97');
 mysql_select_db('indiApp') or die($connect_error);
-$connect=mysqli_connect('us-cdbr-azure-west-c.cloudapp.net:3306','bd377ee286e9fc','4e7a8a97');
+$connect=mysqli_connect('us-cdbr-azure-west-c.cloudapp.net:3306','bd377ee286e9fc','4e7a8a97','indiApp');
 ?>
